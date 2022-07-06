aad5bf6716e2a1974be13f66551dd9f147ff05b4
phoenixguild <106299314+phoenixguild@users.noreply.github.com>
2022-07-06 12:59:49 +0100

    changed the whole idea

--- web.js
+++ web.js
@@ -1,7 +1,17 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
